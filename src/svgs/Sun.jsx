be049fa643cae02b1207176068dd72d69ceba6f0
bobllor <bobllor@yahoo.com>
2025-02-16 23:38:38 -0600

    Updated SVGs folder

--- src/svgs/Sun.jsx
+++ src/svgs/Sun.jsx
@@ -1,4 +1,4 @@
-export default function Sun({ fill='none', color='white' }){
+export default function Sun({ color='white', fill='none' }){
     return (
         <>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={fill}
