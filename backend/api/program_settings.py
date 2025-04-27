99d1a42d6978898665dc83c0fee7f48acb99c10b
bobllor <bobllor@yahoo.com>
2025-04-27 02:44:35 -0500

    Fixed bug with return value

--- backend/api/program_settings.py
+++ backend/api/program_settings.py
@@ -3,8 +3,5 @@ from tkinter.filedialog import askdirectory, askopenfilename
 def get_output():
     '''Sets the output directory of where the label will go.'''
     output_dir = askdirectory()
-    
-    if output_dir == '':
-        return {'status': 'error', 'message': 'EMPTY.INPUT'}
 
     return output_dir
