713a81ecd187286e51b84272f4ab9ed952f0cac5
bobllor <bobllor@yahoo.com>
2025-04-28 00:33:57 -0500

    Moved drag overlay over everything

--- src/components/DragDropOverlay.jsx
+++ src/components/DragDropOverlay.jsx
@@ -1,7 +1,8 @@
 export default function DragDropOverlay(){
     return (
         <>
-        <div className="w-[98%] h-[80%] rounded-[30px] bg-[rgb(107,108,220,.65)] absolute pointer-events-none z-150">
+        <div className="w-[98%] h-[80%] rounded-[30px] bg-[rgb(107,108,220,.65)]
+        absolute pointer-events-none z-1001">
             <div className="h-full w-full flex justify-center items-center">
                 <span className="text-white text-3xl">
                     <strong>Drop file here</strong>
