7e91a7dcb9257ff58146d8fa56898b5b5276a310
bobllor <bobllor@yahoo.com>
2025-05-03 12:51:34 -0500

    Updated props

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,12 @@
 import FileInput from "../components/FileInput"
 import TicketBox from "../components/TicketBox"
 
-export default function Home({handleChange, file, loading, dataRes, showDrag}){
+export default function Home({uploadExcelFile, file, loading, dataRes, showDrag}){
     return (
         <>
             {!loading &&
             <>
-            {!file && <FileInput onFileChange={handleChange} showDrag={showDrag} />}
+            {!file && <FileInput uploadExcelFile={uploadExcelFile} showDrag={showDrag} />}
             {file && <TicketBox props={dataRes} showDrag={showDrag}/>}
             </>}
         </>
