bb6872a9a439e31126dea34c4fb2c9571d87bc67
bobllor <bobllor@yahoo.com>
2025-05-06 00:57:54 -0500

    Improved doc selection on the tabs, SVGs, UX, and CSS changes

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -1,11 +1,14 @@
 import Tab from "./GuideComponents/Tab"
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Hamburger from "../svgs/Hamburger";
 import TOC from "./GuideComponents/TOC";
 import { useDataContext } from "../context/DataContext";
 import TextContent from "./GuideComponents/TextContent";
+import X from "../svgs/X"
+import RightArrow from "../svgs/RightArrow";
+import LeftArrow from "../svgs/LeftArrow";
 
-export default function Guide(){
+export default function Guide({guide}){
     const {tabs, setTabs} = useDataContext();
 
     const [showTOC, setShowTOC] = useState(false);
@@ -18,32 +21,98 @@ export default function Guide(){
         }
     }
 
+    // used for overflow on the tab header.
+    const tabsContainer = useRef(null);
+    const [showScrollArrow, setShowScrollArrow] = useState(false);
+
+    const checkScrollWidth = () => {
+        const tabScrollWidth = tabsContainer.current.scrollWidth;
+        const tabClientWidth = tabsContainer.current.clientWidth;
+        
+        if(tabScrollWidth > tabClientWidth){
+            setShowScrollArrow(true);
+        }else{
+            setShowScrollArrow(false);
+        }
+    }
+
+    useEffect(() => {
+        if(tabsContainer.current != null){
+            checkScrollWidth();
+        }
+    }, [tabsContainer])
+
+    const scrollTabsContainer = (scrollVal) => {
+        if(tabsContainer.current != null){
+            tabsContainer.current.scrollBy({
+                left: scrollVal,
+                behavior: "smooth"
+            })
+        }
+    }
+    
+    const scrollAmount = 150;
+
+    useEffect(() => {
+        if(tabsContainer.current == null) return;
+
+        const observer = new ResizeObserver(() => {
+            checkScrollWidth();
+        })
+
+        observer.observe(tabsContainer.current);
+    
+        return () => observer.disconnect();
+    }, [])
+
     return (
         <>
             <div className="w-[inherit] h-[inherit] bg-gray-400/30 absolute outline-0 
             flex items-center justify-center z-999 text-black backdrop-blur-xs">
                 <div className="bg-white h-[90%] w-[90%] rounded-[10px] 
                 p-3 pt-5 flex-col justify-items-center relative default-background">
-                    <div className="h-10 w-full flex items-center px-2 gap-[2px] overflow-x-auto relative
-                    light-background shadow-[0_3px_8px_1px_rgba(0,0,0,.15)] rounded-[5px]">
-                        <div className="h-[90%] w-10 rounded-[5px] flex justify-center items-center hover:bg-gray-500"
-                        onClick={handleTOCDisplay}>
-                            <Hamburger />
+                    <div className="h-10 w-full flex items-center px-2 relative
+                    light-background shadow-[0_3px_8px_1px_rgba(0,0,0,.15)] rounded-[5px] justify-between">
+                        <div className="flex h-[inherit] justify-center items-center gap-[2px] relative
+                        overflow-x-auto w-[inherit]">
+                            <div className="h-[77%] w-9 rounded-[5px] flex justify-center items-center
+                                hover:bg-gray-500"
+                                onClick={handleTOCDisplay}>
+                                <Hamburger />
+                            </div>
+                            <div className={`${showScrollArrow && `hover:bg-gray-500 rounded-[5px] h-[77%]
+                            flex justify-center items-center`}`}
+                            onClick={() => scrollTabsContainer(-scrollAmount)}>
+                                <LeftArrow fill={showScrollArrow ? "black" : "gray"}/>
+                            </div>
+                            <div className="flex h-[inherit] items-center gap-[2px]
+                            overflow-x-auto hide-scroll w-full"
+                            ref={tabsContainer}>
+                                {
+                                    tabs.map((ele, i) => (
+                                        <Tab tabData={ele} setTabData={setTabs} key={i}/>
+                                    ))
+                                }
+                            </div>
+                            <div className={`${showScrollArrow && `hover:bg-gray-500 rounded-[5px] h-[77%]
+                            flex justify-center items-center`}`}
+                            onClick={() => scrollTabsContainer(scrollAmount)}>
+                                <RightArrow fill={showScrollArrow ? "black" : "gray"}/>
+                            </div>
+                            <div className="flex justify-center items-center rounded-[5px] p-1 hover:bg-gray-500"
+                            onClick={() => guide.setShowGuide(prev => !prev)}>
+                                <X />
+                            </div>
                         </div>
-                        {
-                            tabs.map((ele, i) => (
-                                <Tab tabData={ele} setTabData={setTabs} key={i}/>
-                            ))
-                        }
                     </div>
                     {showTOC && 
                     <div className="bg-white/90 h-70 w-70 absolute shadow-[0_3px_8px_1px_rgba(0,0,0,.15)]
-                    rounded-[5px] overflow-y-auto hide-scroll mt-2">
+                    rounded-[5px] overflow-y-auto hide-scroll mt-2 lg:min-w-100 lg:min-h-100 flex justify-center">
                         <div className="h-[inherit] w-[inherit] p-3">
                             {
                                 tabs.map((ele, i) => (
                                     ele.active &&
-                                    <span className="flex flex-col" key={i}>
+                                    <span className="flex flex-col text-center items-center w-full gap-1" key={i}>
                                         <TOC tabData={ele}/>
                                     </span>
                                 ))
@@ -52,7 +121,7 @@ export default function Guide(){
                     </div>
                     }
                     <div className="h-[inherit] prose overflow-y-auto hide-scroll !min-w-150 !max-w-150 
-                    lg:!min-w-230 lg:!max-w-230 light-background p-5
+                    lg:!min-w-230 lg:!max-w-230 light-background p-5 select-text
                     rounded-[5px] shadow-[0_3px_8px_1px_rgba(0,0,0,.15)] mt-3">
                         {tabs.map((ele, i) => (
                             ele.active &&
