6f08172de18df84e669b313f4bc336f8d624b79d
bobllor <bobllor@yahoo.com>
2025-05-29 21:13:44 -0500

    Updated for final

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { TicketProvider } from './context/TicketContext.jsx'
 import { SettingsProvider } from './context/SettingsContext.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import { AlertsProvider } from './context/AlertsContext.jsx'
 import App from './App.jsx'
 import { DataProvider } from './context/DataContext.jsx'
@@ -17,9 +17,9 @@ createRoot(document.getElementById('root')).render(
           <AlertsProvider>
             <SettingsProvider>
               <ModalProvider >
-                <BrowserRouter>
+                <HashRouter>
                   <App />
-                </BrowserRouter>
+                </HashRouter>
               </ ModalProvider>
             </SettingsProvider>
           </AlertsProvider>
